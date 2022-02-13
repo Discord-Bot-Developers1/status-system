0f4cd84e124301b3ce861a4b56909cdf9ed8bfaa
Discord-Bot-Developers1 <99521910+Discord-Bot-Developers1@users.noreply.github.com>
2022-02-13 11:04:23 +0800

    Add files via upload

---
+++ commands/owner/owner.js
@@ -0,0 +1,14 @@
+module.exports=({
+
+name: "eval",
+aliases: ["e"],
+description: "Eval a command.",
+ usage: "eval <code>",
+ category: "Bot Developer",
+code: ` 
+
+$eval[$message] 
+
+$onlyForIDs[691222873330810891;690390562230108170;]`
+
+})
